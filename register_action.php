ed4cc8f22377caacf9bbca109cd8912543590332
robotanh <robotanh15@gmail.com>
2023-12-02 21:35:11 +0700

    register

--- register_action.php
+++ register_action.php
@@ -12,24 +12,43 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $result = mysqli_query($conn, $checkUsernameQuery);
 
     if (mysqli_num_rows($result) > 0) {
-        // Redirect back to register.html with an error message
+        // Redirect back to register.php with an error message
         header("Location: register.php?error=username_exists");
         exit();
     } else {
         // Insert data into the 'users' table
         $insertQuery = "INSERT INTO users (username, password)
                         VALUES ('$username', '$password')";
-
-        // Execute the SQL query
+        
+        // Execute the INSERT query
         if (mysqli_query($conn, $insertQuery)) {
-            echo "User registered successfully";
-            header("Location: signin.html");
-            exit();
+            // Retrieve the user_id after successful insertion
+            $getUserIDQuery = "SELECT user_id FROM users WHERE username = '$username'";
+            $result = mysqli_query($conn, $getUserIDQuery);
+
+            if ($result && mysqli_num_rows($result) > 0) {
+                $row = mysqli_fetch_assoc($result);
+                $user_id = $row['user_id'];
+
+                // Insert user_id into the 'cv_management' table
+                $insertCVManagementQuery = "INSERT INTO cv_management (user_id, created_date, updated_date)
+                                           VALUES ('$user_id', NOW(), NOW())";
+
+                if (mysqli_query($conn, $insertCVManagementQuery)) {
+                    echo "User registered successfully. User ID: $user_id";
+                    header("Location: signin.html?regist_successfully");
+                } else {
+                    echo "Error inserting into cv_management: " . mysqli_error($conn);
+                }
+            } else {
+                echo "Error retrieving user_id: " . mysqli_error($conn);
+            }
         } else {
-            echo "Error: " . $insertQuery . "<br>" . mysqli_error($conn);
+            echo "Error inserting user data: " . mysqli_error($conn);
         }
     }
 
     mysqli_close($conn);
+    
 }
 ?>
